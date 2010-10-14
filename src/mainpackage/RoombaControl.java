56b1ac5069156aff0332ea761107fa8ac5bc833e
iamjt <khoojt@gmail.com>
2010-10-14 17:48:42 +0800

    Royal Roomba Manager:
    1.Publishing and Subscription to MQTT done.
    Messages to Roomba 1 should be published to topic "toroomba1"
    Messages to Roomba 2 should be published to topic "toroomba2"
    Messages by Roomba 1 will be published to topic "byroomba1"
    Messages by Roomba 1 will be published to topic "byroomba1"
    
    2. RoombaControl
    List of Roomba Command Functions Implemented
    Publish this to a to<roomba> channel to command roomba to perform action
    
    DISCONNECT - DC the roomba
    
    ACCELERATE - Move the roomba faster in the forward direction, needs to overcome
    forward velocity first.
    
    DECELERATE - Move the roomba faster in the backward direction, needs to overcome
    backward velocity first.
    
    BOOST - Accelerate at 3 times normal rate
    
    MAX_SPEED - Set Speed Cap at 500 mm/s
    
    NORMAL_SPEED - Set Speed Cap at 400mm/s
    
    STOP - Stop the roomba
    
    TURN_LEFT - Steer Left while moving forward (Right if backwards), speed will decrease.
    
    TURN_RIGHT - Steer Right while moving forward, (Left if backwards), speed will decrease
    
    SLOW_DOWN - Slowly decreases the speed to 0. 500 to 0
    
    SPIN - Spins 180 degrees clockwise
    
    RESET - Reconnects the roomba in case of wheelie

---
+++ src/mainpackage/RoombaControl.java
@@ -0,0 +1,216 @@
+package mainpackage;
+import roombacomm.*;
+
+public class RoombaControl {
+	
+	public RoombaCommSerial roombacomm;
+	public String comPort;
+	public int velocity;
+	public int radius;
+	public boolean left;
+	public boolean right;
+	public int turnConstant;
+	public int leftFactor ;
+	public int rightFactor;
+	public int speedCap;
+	
+	public RoombaControl(String portNumber){
+		
+		//INstantiate variables
+		comPort = portNumber;
+		roombacomm = new RoombaCommSerial(true);
+		velocity = 0;
+		radius = 0;
+		leftFactor = 0;
+		rightFactor = 0;
+		left = false;
+		right = false;
+		speedCap = 400;
+		turnConstant = 100;
+		
+		//print debug msg if cant connect
+		if( !roombacomm.connect( portNumber ) ) {
+			System.out.println("Couldn't connect to "+portNumber);
+            System.exit(1);
+        }
+
+	        
+		// connect to port
+		roombacomm.connect( portNumber );
+	
+		// startup the roomba
+		System.out.println("starting up roomba at "+ comPort);
+		roombacomm.startup();
+		roombacomm.control();
+		roombacomm.pause(50);
+		roombacomm.reset();
+		
+	}
+	
+	//Reset the roomba incase of wheelie
+	public void resetRoomba(){
+		roombacomm.startup();
+		roombacomm.control();
+		roombacomm.pause(50);
+		roombacomm.reset();
+	}
+
+	
+	//Function command the roomba
+	public void roombaAction(String action){
+		
+		//LIST OF COMMANDS
+		//----------------
+		//DISCONNECT
+		//ACCELERATE
+		//DECELERATE
+		//BOOST
+		//MAX_SPEED
+		//NORMAL_SPEED
+		//STOP
+		//TURN_LEFT
+		//TURN_RIGHT
+		//SLOW_DOWN
+		//SPIN
+		
+		//Set direction to straight ahead
+		if(!action.contains("TURN")){
+			radius = 0x8000;
+		}
+		
+		if(action.equals("RESET")){
+			resetRoomba();
+		}
+		
+		//Action to dc roomba
+		if(action.equals("DISCONNECT")){
+			roombacomm.stop();
+			roombacomm.disconnect();
+		}
+		
+		//Action to accelerate forward
+		//Forward velocity is increased at a constant rate of 11mm/s
+		//and a variable increase at set 10% of the velocity
+		//Will have to cancel out any backward motion first before accelerating forward
+		//MAX roomba speed is at 500mm/s
+		if(action.equals("ACCELERATE")){
+			
+			//forward velocity caps at speed cap
+			if(velocity < speedCap){
+				velocity += 11;
+				
+				if((0.2*velocity) < 0 ){
+					velocity -= (int)(0.2*velocity);
+				}else{
+					velocity += (int)(0.2*velocity);
+				}
+			}
+		}
+		
+		//Action to move accelerate backwards
+		//will have to cancel out the forward velocity first before
+		//roomba can move backwards
+		if(action.equals("DECELERATE")){
+			
+			if(velocity > -speedCap){
+				velocity -= 11;
+				
+				if((0.2*velocity) > 0 ){
+					velocity -= (int)(0.2*velocity);
+				}else{
+					velocity += (int)(0.2*velocity);
+				}
+			}
+		}
+		
+		
+		//Nitro pack boasts increased acceleration by as much as
+		//an additional 20%
+		if(action.equals("BOOST")){
+			
+			//forward velocity caps at 400mm/s
+			if(velocity < speedCap){
+				velocity +=11;
+				velocity += (int)(0.3*velocity);
+			}
+		}
+		
+		//Sets max speed cap to 500;
+		if(action.equals("MAX_SPEED")){
+			speedCap = 500;
+		}
+		
+		//Set speed cap to 400;
+		if(action.equals("NORMAL_SPEED")){
+			speedCap = 400;
+		}
+		
+		//Stop the roomba
+		if(action.equals("STOP")){
+			velocity = 0;
+			roombacomm.stop();
+		}
+		
+		//TURNING
+		//turning has 2 components
+		//a CONSTANT turnConstant to regulate the initial radius (set at 1 roomba unit)
+		//And the speed that the Roomba is moving at
+		//The larger the speed, the larger the radius of the turn
+		//Turning will also slow down the roomba at a rate of 5% per call
+		//5% Might be too big, need to test
+		
+		//Turns left
+		if(action.equals("TURN_LEFT")){
+			
+			radius = -turnConstant-Math.abs(velocity);
+			velocity *= 0.95;
+			
+			left = true;
+			leftFactor += 5;
+		}/*else{
+			left = false;
+			leftFactor = 0;
+		}*/
+		
+		//turns right
+		if(action.equals("TURN_RIGHT")){
+			
+			radius = turnConstant+Math.abs(velocity);
+			velocity *= 0.95;
+			right = true;
+			rightFactor += 5;
+		}/*else{
+			right = false;
+			rightFactor = 0;
+		}*/
+		
+		//Slows down the roomba
+		if(action.equals("SLOW_DOWN")){
+			
+			if(velocity < 10){
+				
+				velocity = 0;
+			}
+			
+			if(velocity>0){
+				roombaAction("DECELERATE");
+			}
+			
+			if(velocity<0){
+				roombaAction("ACCELERATE");
+			}
+		}
+		
+		//Rooomba command to move;
+		if(!action.equals("SPIN")){
+			roombacomm.drive(-velocity, radius);
+		}else{
+			roombacomm.speed = 500;
+			roombacomm.spinRight(180);
+			roombacomm.speed = velocity;
+		}
+		
+		//publish roomba stats
+		RoyalRoombaManager.publish(comPort, "VELOCITY:"+velocity);
+	}
+}
---
+++ src/mainpackage/RoyalRoombaManager.java
@@ -0,0 +1,84 @@
+package mainpackage;
+import com.ibm.mqtt.*;
+
+public class RoyalRoombaManager implements MqttSimpleCallback {
+	
+	public static IMqttClient mqttclient;
+	public static RoombaControl roomba1;
+	//public static RoombaControl roomba2;
+	public static String port1 = "COM40";
+	public static String port2 = "COM41";
+	
+	public static void main(String args[]){
+		
+		//Start MQTT client
+		RoyalRoombaManager rrm = new RoyalRoombaManager();
+		rrm.startMQTT();
+		
+		//Instantiate roombas
+		roomba1 = new RoombaControl(port1);
+		//roomba2 = new RoombaControl(port2);		
+	}
+	
+	public void startMQTT(){
+		
+		try{
+			mqttclient = MqttClient.createMqttClient("tcp://localhost@1883", null);
+			mqttclient.connect("1", true, (short)5);
+			mqttclient.registerSimpleHandler(this);
+		}catch(Exception e){
+			e.printStackTrace();
+		}		
+		
+		String[] stringArray = {"roomba1", "roomba2"};
+		int[] numArray = {1,1};
+		
+		try{
+			mqttclient.subscribe(stringArray, numArray);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void connectionLost(){
+		
+	}
+	
+	public void publishArrived(String topicName, byte[] payload, int qos, boolean retain){
+		
+		if(topicName.equals("toroomba1")){
+			roomba1.roombaAction(new String(payload));
+		}else{
+			
+			if(topicName.equals("toroomba2")){
+				System.out.println(topicName);
+				//roomba2.roombaAction(new String(payload));
+			}
+		}
+	}
+	
+	//Pushes roomba messages to the MQTT broker
+	public static void publish(String portname,String thismessage){
+		
+		System.out.println(thismessage);
+		
+		try{
+			//Publish to roomba1
+			if(portname.equals(port1)){
+				mqttclient.publish("byroomba1", thismessage.getBytes(), 1, false);
+			}
+			
+			//Publish to roomba2
+			if(portname.equals(port2)){
+				mqttclient.publish("byroomba2", thismessage.getBytes(), 1, false);
+			}
+			
+			//Publish to server
+			if(portname.equals("server")){
+				
+			}
+		}catch(Exception e){
+	
+		}
+	}
+}
